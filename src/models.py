6d7ac9e9e9ecb745a3fb7ac0766ec6743531552c
CatalinaScheleff <122483646+CatalinaScheleff@users.noreply.github.com>
2023-04-16 08:02:08 +0000

    modelo

--- src/models.py
+++ src/models.py
@@ -7,23 +7,48 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    clave = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Personaje(Base):
+    __tablename__ = 'personaje'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    personaje_id = Column(Integer, primary_key=True)
+    personaje_name = Column(String(250), nullable=False)
+
+class Location(Base):
+    __tablename__ = 'location'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    location_id = Column(Integer, primary_key=True)
+    location_name = Column(String(250), nullable=False)
+
+class Personaje_Favorito(Base):
+    __tablename__ = 'personaje_favorito'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    personaje_id = Column(Integer, ForeignKey('personaje.id'))
+    personaje = relationship(Personaje)
+
+class Location_Favorito(Base):
+    __tablename__ = 'location_favorito'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    location_id = Column(Integer, ForeignKey('location.id'))
+    location = relationship(Location)
 
     def to_dict(self):
         return {}
